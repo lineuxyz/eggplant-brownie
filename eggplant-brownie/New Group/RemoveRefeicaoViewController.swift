94f4db1150b3574d7bf5bbb21be0c4e4ced248b9
Lineu Pastorelli <lineualberto@gmail.com>
2020-05-10 15:48:43 -0300

    add all controller to the application

--- eggplant-brownie/New Group/RemoveRefeicaoViewController.swift
+++ eggplant-brownie/New Group/RemoveRefeicaoViewController.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Lineu Alberto. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RemoveRefeicaoViewController {
+    
+    let controller: UIViewController
+    
+    init(controller: UIViewController) {
+        self.controller = controller
+    }
+    
+    func exibe(_ refeicao: Refeicao, handler: @escaping (UIAlertAction) -> Void) {
+        let alerta = UIAlertController(title: refeicao.nome, message: refeicao.detalhes(), preferredStyle: .alert)
+        let botaoCancelar = UIAlertAction(title: "cancelar", style: .cancel)
+        alerta.addAction(botaoCancelar)
+        let botaoRemover = UIAlertAction(title: "remover", style: .destructive, handler: handler)
+        alerta.addAction(botaoRemover)
+        
+        controller.present(alerta, animated: true, completion: nil)
+    }
+}
--- eggplant-brownie/New Group/ViewController.swift
+++ eggplant-brownie/New Group/ViewController.swift
@@ -8,16 +8,122 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol AdicionaRefeicaoDelegate {
+    func add(_ refeicao: Refeicao)
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AdicionaItensDelegate {
     
-    @IBOutlet var nomeTextField: UITextField!
-    @IBOutlet var felicidadeTextField: UITextField!
+    
+    // MARK: - IBOutlet
+    
+    
+    @IBOutlet weak var itensTableView: UITableView?
+    
+    // MARK: - Atributos
+    
+    var delegate: AdicionaRefeicaoDelegate?
+    var itens: [Item] = []
+    var itensSelecionados: [Item] = []
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var nomeTextField: UITextField?
+    @IBOutlet var felicidadeTextField: UITextField?
+    
+    
+    // MARK: - View life cycle
+    
+    override func viewDidLoad(){
+        let botaoAdicionaItem = UIBarButtonItem(title: "adicionar", style: .plain, target: self, action: #selector(adicionarItem))
+        navigationItem.rightBarButtonItem = botaoAdicionaItem
+        recuperaItens()
+    }
+    
+    func recuperaItens() {
+        itens = ItemDao().recupera()
+    }
+    
+    @objc func adicionarItem() {
+        let adicionarItensViewController = AdicionarItensViewController(delegate: self)
+        navigationController?.pushViewController(adicionarItensViewController, animated: true)
+    }
+    
+    func add(_ item: Item) {
+        itens.append(item)
+        ItemDao().save(itens)
+        if let tableView = itensTableView {
+            tableView.reloadData()
+        } else {
+            Alerta(controller: self).exibe(mensagem: "Não foi possível atualizar a tabela")
+        }
+        
+    }
+    
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->
+        Int {
+            return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+            
+            let linhaDaTabela = indexPath.row
+            let item = itens[linhaDaTabela]
+            
+            celula.textLabel?.text = item.nome
+            
+            return celula
+    }
+    
+    // MARK: - UiTableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+            
+            let linhaDaTabela = indexPath.row
+            
+            itensSelecionados.append(itens[linhaDaTabela])
+        } else {
+            celula.accessoryType = .none
+            
+            let item = itens[indexPath.row]
+            if let position = itensSelecionados.firstIndex(of: item) {
+                itensSelecionados.remove(at: position)
+            }
+        }
+    }
+    
+    func recuperaRefeicaoDoFormulario() -> Refeicao? {
+        guard let nomeDaRefeicao = nomeTextField?.text else {
+            return nil
+        }
+        
+        guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
+            return nil
+        }
+        
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
+        
+        return refeicao
+    }
+    
+    // MARK: - IBActions
     
     @IBAction func adicionar() {
-        let nome = nomeTextField.text
-        let felicidade = felicidadeTextField.text
         
-        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
+        if let refeicao = recuperaRefeicaoDoFormulario() {
+            delegate?.add(refeicao)
+            navigationController?.popViewController(animated: true)
+        } else {
+            Alerta(controller: self).exibe(mensagem: "Erro ao ler dados do formulário")
+        }
     }
 }
 
